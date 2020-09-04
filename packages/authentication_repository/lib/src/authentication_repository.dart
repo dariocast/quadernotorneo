3297b27aabbdbc79a4dc6fd8ccebf2208b9e7e16
Dario Castellano <>
2020-09-04 13:29:33 +0200

    add authentication

---
+++ packages/authentication_repository/lib/src/authentication_repository.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+import 'package:crypto/crypto.dart';
+import 'dart:convert'; // for the utf8.encode method
+
+import 'package:meta/meta.dart';
+import 'package:http/http.dart' show Client;
+
+enum AuthenticationStatus { unknown, authenticated, unauthenticated }
+
+class AuthenticationRepository {
+  Client client = Client();
+  final _authUrl = 'https://dariocast.altervista.org/fantazama/api/admin/auth.php';
+  final _controller = StreamController<AuthenticationStatus>();
+
+  Stream<AuthenticationStatus> get status async* {
+    await Future<void>.delayed(const Duration(seconds: 1));
+    yield AuthenticationStatus.unauthenticated;
+    yield* _controller.stream;
+  }
+
+  Future<void> logIn({
+    @required String username,
+    @required String password,
+  }) async {
+    var digest = sha1.convert(utf8.encode(password));
+    final response =
+        await client.post(_authUrl, body: {'username': username, 'password': digest});
+    if (response.statusCode == 200) {
+      _controller.add(AuthenticationStatus.authenticated);
+    } else {
+      _controller.add(AuthenticationStatus.unauthenticated);
+    }
+  }
+
+  void logOut() {
+    _controller.add(AuthenticationStatus.unauthenticated);
+  }
+
+  void dispose() => _controller.close();
+}
