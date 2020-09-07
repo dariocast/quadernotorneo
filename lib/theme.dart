0f18feb40022df62d670c463a66762b94160ae12
Dario Castellano <dariocastellano18@gmail.com>
2020-09-07 17:39:22 +0200

    theme

---
+++ lib/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class QuadernoTheme {
+  static ThemeData get themeData {
+    return ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.green,
+        accentColor: Colors.amber);
+  }
+}
